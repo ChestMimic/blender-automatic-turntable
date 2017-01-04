55a91a85cf60695307af78fb34a3a0d003e5ead9
Fitzgibbon <ibbolia@gmail.com>
2017-01-04 10:26:19 -0500

    Implemented lambda function for BBox calculation

--- BoundingBox.py
+++ BoundingBox.py
@@ -2,37 +2,39 @@ from mathutils import Vector
 
 class BoundingBox:
 	def __init__(self, lst=[], globalPos = (0.0,0.0,0.0)):
-		self.xMin = None
-		self.xMax = None
-		self.yMin = None
-		self.yMax = None
-		self.zMin = None
-		self.zMax = None
+		xMin = None
+		xMax = None
+		yMin = None
+		yMax = None
+		zMin = None
+		zMax = None
 
 		#Obtain bounding box in an object's local space
 		for ob in lst:
 			for b in ob.bound_box:
-				print(ob.location)
+				loc, rot, scale = ob.matrix_world.decompose();
 
-				if self.xMax is None or ((Vector(b)[0] + ob.location[0]) > self.xMax):
-					self.xMax = Vector(b)[0] + ob.location[0]
-				if self.xMin is None or ((Vector(b)[0] + ob.location[0]) < self.xMin):
-					self.xMin = Vector(b)[0] + ob.location[0]
+				globalPos = lambda i: Vector(b)[i] + loc[i]
 
-				if self.yMax is None or ((Vector(b)[1] + ob.location[1]) > self.yMax) :
-					self.yMax = Vector(b)[1] + ob.location[1]
-				if self.yMin is None or ((Vector(b)[1] + ob.location[1]) < self.yMin):
-					self.yMin = Vector(b)[1] + ob.location[1]
+				if xMax is None or (globalPos(0) > xMax):
+					xMax = globalPos(0)
+				if xMin is None or (globalPos(0) < xMin):
+					xMin = globalPos(0)
 
-				if self.zMax is None or ((Vector(b)[2] + ob.location[2]) > self.zMax):
-					self.zMax = Vector(b)[2] + ob.location[2]
-				if self.zMin is None or ((Vector(b)[2] + ob.location[2]) < self.zMin):
-					self.zMin = Vector(b)[2] + ob.location[2]
+				if yMax is None or (globalPos(1) > yMax) :
+					yMax = globalPos(1)
+				if yMin is None or (globalPos(1) < yMin):
+					yMin = globalPos(1)
 
-		self.xMid = (self.xMin + self.xMax)/2
-		self.yMid = (self.yMin + self.yMax)/2
-		self.zMid = (self.zMin + self.zMax)/2
+				if zMax is None or (globalPos(2) > zMax):
+					zMax = globalPos(2)
+				if zMin is None or (globalPos(2) < zMin):
+					zMin = globalPos(2)
 
-		self.minimum = (self.xMin, self.yMin, self.zMin)
-		self.maximum = (self.xMax, self.yMax, self.zMax)
-		self.midpoint = (self.xMid, self.yMid, self.zMid)
+		xMid = (xMin + xMax)/2
+		yMid = (yMin + yMax)/2
+		zMid = (zMin + zMax)/2
+
+		self.minimum = (xMin, yMin, zMin)
+		self.maximum = (xMax, yMax, zMax)
+		self.midpoint = (xMid, yMid, zMid)
